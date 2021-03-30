93e72c6aeded1680dd7ba8abf16a541526779c1d
cherkashin <alexander.cherkashin@sibit.tech>
2021-03-30 21:28:14 +0700

    comment

--- push_new_version.sh
+++ push_new_version.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+COMMENT="${1:-}"
+VERSION=`cat relocation_helper/__init__.py|grep '__version__'|sed -e 's/$/\nprint(__version__)/g'|poetry run python`
+echo "Version: $VERSION"
+git tag -l|grep -q "$VERSION" && echo "Version $VERSION already exists, please change version in __init__.py" && exit -1
 poetry run pytest
-[[ ! "$comment" ]] && read -p 'Please set commit comment: ' comment
-VERSION=`cat relocation_helper/__init__.py|grep '__version__'||sed -e 's/$/\nprint(__version__)/g'|poetry run python`
-git tag -a $version -m "$comment"
+[[ ! "$COMMENT" ]] && read -p 'Please set commit comment: ' COMMENT
 git add --all
 git commit -m "comment"
+git tag -a $VERSION -m "$COMMENT"
+git push origin master
+git push origin $VERSION
+echo "`date`: SUCCESS"
